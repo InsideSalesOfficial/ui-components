a19a38b74432ccdceb46c80f29e2ac8f6a7bb08c
Morgan Nesbitt <morgan.nesbitt@insidesales.com>
2019-10-15 10:53:02 -0600

    helper for storybooks

--- src/components/styles/index.js
+++ src/components/styles/index.js
@@ -2,3 +2,7 @@ export * from './boxShadows';
 export * from './colors';
 export * from './scrollbars';
 export * from './typography';
+
+export function generateFlexedThemeBackground(props, { height }) {
+  return { background: props.theme.primary01, height: height, display: 'flex', alignItems: 'center' };
+};
