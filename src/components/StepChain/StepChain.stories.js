6f43446ba59b8388704a1c4b55663e53f9fc58de
Bob Shelline <bob.shelline@XANT.ai>
2020-01-16 16:54:07 -0700

    Adjust step chain story

--- src/components/StepChain/StepChain.stories.js
+++ src/components/StepChain/StepChain.stories.js
@@ -11,7 +11,7 @@ import { generateFlexedThemeBackground } from '../styles/index.js';
 
 const ThemeComponent = (props) => (
   <ThemeProvider theme={props.theme}>
-    <div style={generateFlexedThemeBackground(props, { width: '100%', height: '100px', paddingLeft: '8px' })}>
+    <div style={generateFlexedThemeBackground(props, { width: '90%', height: '100px', padding: '0 8px' })}>
       {props.children}
     </div>
   </ThemeProvider>
